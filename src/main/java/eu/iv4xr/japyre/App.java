fe08b152d41ff4ddf78e7673171ab38524ff39ef
wooshrow <s.w.b.prasetya@uu.nl>
2022-09-27 09:37:03 +0200

    initial commit

---
+++ src/main/java/eu/iv4xr/japyre/App.java
@@ -0,0 +1,13 @@
+package eu.iv4xr.japyre;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
