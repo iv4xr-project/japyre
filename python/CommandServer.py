ad156bc666384d63db2ce3faf307e88e048ca1c0
wooshrow <s.w.b.prasetya@uu.nl>
2022-09-30 20:17:04 +0200

    refactoring Py-side CommandServer

--- python/CommandServer.py
+++ python/CommandServer.py
@@ -3,64 +3,90 @@ import json
 from typing import Any, Callable, Dict
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 9999        # Port to listen on (non-privileged ports are > 1023)
-
+PORT = 9999         # Port to listen on (non-privileged ports are > 1023)
+  
 debug = True 
 
-def deployCommandServer(host:str,
-                        port:int,
-                        commandInterpreter : Callable[[Dict,Dict],Any],
-                        receiveBufferSize=4096) -> None :
+class CommandServer:
     '''
-    Deploy a command-server at the given ip-address and port. The server
-    accept commands sent by a client. A command is a pair (cmd,arg) where
-    cmd is a string specifying the command name, and arg is a Json-string
-    representing the argument of the command.
-
-    This function takes a command-interpreter f as an argument. Upon receiving
-    a pair (cmd,arg) from the client, f(cmd,arg) is invoked. The function is
-    assumed to produce a Json-string, which is then sent back to the client.
-
-    The server closes when the client asks it to close. 
-
-    Parameters:
-      host (string): the ip when this server will be hosted
-      port (int):    the port number
-      commandInterpreter: is a function that takes two string cmd and arg as arguments.
-                          The arg is assumed to be a Json-string.
-                          The function is assumed to return a Json-string.
+    This class implements a command-server. To use it, first create an instance
+    of this class, then run the method deploy().
     '''
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((host,port))
-        s.listen()
-        print(f"> Starting a sever at {host}:{port}")
-        clientSocket, addr = s.accept()
-        with clientSocket:
-            print(f"> CONNECTED by {addr}")
-            while True:
-                received = clientSocket.recv(receiveBufferSize) 
-                if not received:
-                    print("> CLOSING the server.")
-                    break
-                text = received.decode("utf-8")
-                myjson = json.loads(text)
-                cmd = myjson["cmd"] # the command-string
-                arg = myjson["arg"] # the arg-object, represented as a nested Dictionary
-                if debug :
-                    #print(f"> receiving {text}")
-                    print(f"> receiving cmd: {cmd}")
-                    print(f">           arg: {arg}")
-                    
-                # interpret the command:
-                result =  commandInterpreter(cmd,arg)   
-                resultJson = json.dumps(result) 
-                # send result back to the client:
-                # need to add that newline-char at the end, else the client does not
-                # know that the string that was sent has ended:
-                clientSocket.sendall(bytes(resultJson  + "\n",encoding="utf-8"))
-                if debug :
-                    print(f"> sending {resultJson}")
-        s.close()
+
+    def __init__(self, host:str, port:int):
+        '''
+        The constructor. When created, the server does not run yet.
+        The method deploy() will deploy/run the server; it will then be bound
+        to the given ip (host) and port.
+
+        Parameters:
+        host (string): the ip where this server will be hosted
+        port (int):    the port number
+        '''
+        self.host = host
+        self.port = port
+        self.commandInterpreter = None
+
+    def attachInterpreter(self, commandInterpreter : Callable[[Dict,Dict],Any]) :
+        ''' 
+        Attach a function f that will act as an interpreter "commands" received by this server.
+        This f that takes two string cmd and arg as arguments. 
+        The arg is assumed to be a Json-string.
+        
+        The function f is assumed to return a Json-string.
+        '''
+        self.commandInterpreter = commandInterpreter
+
+
+    def deploy(self, receiveBufferSize=4096) -> None :
+        '''
+        Deploy this command-server at the ip-address and port specified by
+        self.host and self.port. This method will just run in a forever-cycle (until
+        the client asks it to close) accepting "commands" sent by a client,
+        interpreting it, and then sends to response back to the client.
+        
+        A command is a pair (cmd,arg) where
+        cmd is a string specifying the command name, and arg is a Json-string
+        representing the argument of the command.
+
+        The function f attached in self.command-interpreter is used to interpret
+        incoming commands. Upon receiving
+        a pair (cmd,arg) from the client, f(cmd,arg) is invoked. The function is
+        assumed to produce a Json-string, which is then sent back to the client.
+
+        The server closes when the client asks it to close. 
+        '''
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((self.host,self.port))
+            s.listen()
+            print(f"> Starting a sever at {self.host}:{self.port}")
+            clientSocket, addr = s.accept()
+            with clientSocket:
+                print(f"> CONNECTED by {addr}")
+                while True:
+                    received = clientSocket.recv(receiveBufferSize) 
+                    if not received:
+                        print("> CLOSING the server.")
+                        break
+                    text = received.decode("utf-8")
+                    myjson = json.loads(text)
+                    cmd = myjson["cmd"] # the command-string
+                    arg = myjson["arg"] # the arg-object, represented as a nested Dictionary
+                    if debug :
+                        #print(f"> receiving {text}")
+                        print(f"> receiving cmd: {cmd}")
+                        print(f">           arg: {arg}")
+                        
+                    # interpret the command:
+                    result =  self.commandInterpreter(cmd,arg)   
+                    resultJson = json.dumps(result) 
+                    # send result back to the client:
+                    # need to add that newline-char at the end, else the client does not
+                    # know that the string that was sent has ended:
+                    clientSocket.sendall(bytes(resultJson  + "\n",encoding="utf-8"))
+                    if debug :
+                        print(f"> sending {resultJson}")
+            s.close()
 
 
 # just for testing:
@@ -69,4 +95,6 @@ if __name__ == '__main__':
         xxx = { "id":0, "name":"Batman" }
         return xxx
 
-    deployCommandServer(HOST,PORT,testCmdInterpreter)        
+    server = CommandServer(HOST,PORT)
+    server.attachInterpreter(testCmdInterpreter)
+    server.deploy()        
--- python/RLServer.py
+++ python/RLServer.py
@@ -3,56 +3,89 @@ import json
 from typing import Any, Callable, Dict, List
 
 class Model:
+    def mkFreshModel(self,modelConfig:Dict) :
+        ''' 
+        Make a fresh untrained model with the given model-configuration. 
+        This method returns thus an instance of Model.
+        '''
+        print(">>> Implement this method.")
+        raise
+
     def save(self, filename:str) -> None :
+        ''' Save this model to a file. '''
         print(">>> Implement this method.")
         raise
 
-    def load(self, filename:str) -> None :
+    def load(self, filename:str) :
+        ''' Load a model from a file and return the model. '''
         print(">>> Implement this method.")
         raise
 
+    def setTrainingAlgorithm(self, trainingConf:Dict) -> None :
+        ''' 
+        Associate a training algorithm to this model, configured to the given 
+        configuration. Alternatively, if the model already comes with its own
+        training algorithm, this method reconfigures to according to the given
+        config.
+        '''
+        print(">>> Implement this method.")
+        raise
+  
     def getNextTrainedAction(self, currentState:Dict) -> str :
+        ''' 
+        Given the current state, this method consults the model to return the best action 
+        to do next (according to the model, and whatever its action selection policy is).
+        '''
         print(">>> Implement this method.")
         raise
 
-class ModelTrainer:
-    def getModel(self) -> Model :
-        print(">>> Implement this method.")
-        raise
-    
-    def configure(self, conf:Dict) -> None :
-        print(">>> Implement this method.")
-        raise
-
-    def applyReward(self, action:str, newstate:Dict, reward:float) -> None :
+    def getNextActionToTry(self, currentState:Dict, possibleActions:List[str]) -> str :
+        '''
+        This method is for training. It asks the model to decide which actions it wants
+        to do next for the purpose of training/learning.
+        '''
         print(">>> Implement this method.")
         raise
 
-    def getNextAction(self, currentState:Dict, possibleActions:List[str]) -> str :
+    def applyReward(self, oldState:Dict, action:str, newstate:Dict, reward:float) -> None :
+        '''
+        Incoporate the learning of executing the given action, on the oldstate, leading
+        to the newstate, and receiving the given reward in doing so.
+        '''
         print(">>> Implement this method.")
         raise
 
 
-def deployTrainingServer(trainer:ModelTrainer, host:str , port:int,receiveBufferSize=4096) -> None :
-    def interpret(cmd,arg):
-        retval = "OK__"
-        if cmd == "START_TRAINING" : 
-                trainer.configure(arg)
-        elif cmd == "SAVE"  :
-                filename = json.dumps(arg)["filename"]
-                trainer.getModel().save(filename)
-        elif cmd == "GET_NEXTACTION" :
-                observation     = arg["st"]  # current observation/state
-                previousAction  = arg["prev"] # the previous action that leads to the current state
-                reward          = arg["rew"]  # the reward given upon reaching the current state
-                possibleActions = arg["opts"] # list of next-actions that are possible on the current state
-                trainer.applyReward(previousAction,observation,reward)
-                nextAction = trainer.getNextAction(observation,possibleActions)
-                retval = nextAction
+class TrainingServer:
+    def __init__(self, host:str, port:int):
+        self.host = host
+        self.port = port
+        self.model   = None
+        self.trainer = None
 
-        return retval
-    print("> About to deploy a model-training-server.")        
-    cmdServer = CommandServer.deployCommandServer(host,port,interpret,receiveBufferSize)
+    def deployTrainingServer(trainer:ModelTrainer,receiveBufferSize=4096) -> None :
+        def interpret(cmd,arg):
+            retval = "OK__"
+            if cmd == "MK_FRESH_MODEL" : 
+                    trainer.configure(arg)
+
+            if cmd == "START_TRAINING" : 
+                    trainer.configure(arg)
+            elif cmd == "SAVE"  :
+                    filename = json.dumps(arg)["filename"]
+                    trainer.getModel().save(filename)
+            elif cmd == "GET_NEXTACTION" :
+                    observation     = arg["st"]  # current observation/state
+                    previousAction  = arg["prev"] # the previous action that leads to the current state
+                    reward          = arg["rew"]  # the reward given upon reaching the current state
+                    possibleActions = arg["opts"] # list of next-actions that are possible on the current state
+                    trainer.applyReward(previousAction,observation,reward)
+                    nextAction = trainer.getNextAction(observation,possibleActions)
+                    retval = nextAction
+
+            return retval
+        print("> About to deploy a model-training-server.")        
+        cmdServer = CommandServer.deployCommandServer(host,port,interpret,receiveBufferSize)
 
 
 def deployModelServer(model:Model, host:str, port:int, receiveBufferSize=4096) -> None :
