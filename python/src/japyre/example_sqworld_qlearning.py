d131be281ea569f6a05e0156a33ea9b88307f74d
wooshrow <s.w.b.prasetya@uu.nl>
2022-10-02 16:43:21 +0200

    improving docs

--- python/src/japyre/example_sqworld_qlearning.py
+++ python/src/japyre/example_sqworld_qlearning.py
@@ -2,76 +2,64 @@ from qlearning   import Qlearning
 from sqworld_env import SqWorldEnv
 import gymenv_client
 
+# This demonstrates an example of applying a Python-side RL algorithm
+# on a Gym-env in Java.
+#
+# The Gym-env is the SquareWorld gym in Java. You first need to run it
+# as a server, see the class SquareWorldGymServer in Java. Run the main
+# of that class.
+#
+# Then you can run this class. It will demonstrate the use of Qlearning
+# to learn how to do actions on the SquareWorld that would guide a 
+# hypothetical robot there to reach a goal location (top-right corner of
+# the world). It is not a difficult problem to solve for RL, but it demostrates
+# the Python-Java connection.
+#
+
 gymenv_client.debug = False
+
+# Run a Python-side client that would connect to the Java-side server of
+# SquareWorld Gym. This client then also provides a Gym-env like interface
+# for us, so we can treat it as if it is an all-Python Gym-env without being
+# aware that under the hood it connects to a Java-side.
 env = SqWorldEnv(6) # 6 is the size of the sq-world as set at the Java-side
+
+# Create an instance of Qlearning algorithm. You need to configure it with
+# the number of states and actions it has to deal with:
 N = env.size
 N_plus_2 = N+2
 qalg = Qlearning(N_plus_2*N_plus_2, len(env.javaGym.actionSpace))
 #qalg.alpha = 1
-print("====== Training...")
-maxSteps = 2000
 def convertToIndex(obs) :
     x = obs[0]
     y = obs[1]
     return ((x+1) * N_plus_2) + y +1
-k = 0
-stepCountInEpisode = 0
-episode = 0
-o = env.reset()
-while k < maxSteps:
-    o_ = convertToIndex(o)
-    #print(f"### o={o}, o_={o_}")
-    action = qalg.getNextAction(o_)
-    #print(f"### action = {action}")
-    nextObs,reward,done,i = env.step(action) 
-    nextO_ = convertToIndex(nextObs)
-    #print(f"### next={nextObs}, next_={nextO_}")
-    qalg.applyReward(o_,action,nextO_,reward)
-    debugo = o
-    o = nextObs
-    if done :
-        print(f">> Episode {episode}, #steps={stepCountInEpisode}, reward={reward}")
-        episode = episode + 1
-        o = env.reset()
-        stepCountInEpisode = 0
-        #if (reward > 0) : 
-        #    print(f"### o={debugo}, o_={o_}, next={nextObs}, next_={nextO_}, rw={reward}")
-        #    print(f"### updated enrty={qalg.qtable[o_][action]}")
-        #    break
-        
-        
-    else:
-        stepCountInEpisode = stepCountInEpisode + 1
-    k = k + 1
+qalg.stateIndexer = convertToIndex    
 
-print("====== Training has finished.")
-for y in range(N_plus_2):
-    y_ = N_plus_2 - y - 1
-    y__ = y_ - 1
-    for x in range(N_plus_2) :
-        x__ = x - 1
-        index = x * N_plus_2 + y_ 
-        vals = qalg.qtable[index]
-        print(f"q({x__},{y__})={vals}")
+# Ok.. now we can run the Q-learning algorithm to train it:
+print("====== Training...")
+qalg.learn(env,maxNumberOfSteps=2000,verbose=True)
 
+# Training is done, now we obtain a sequence of action that
+# use the trained model:
 print("====== Demonstrating trained skill ...")
 # now showing how to use the learned information
-k = 0
-done = False
-o = env.reset()
-print(f">> Robot initial position: {o}")    
-while (not done) and k < 10 :
-    o_ = convertToIndex(o)
-    #print(f"### o={o}, o_={o_}")
-    action = qalg.getNextTrainedAction(o_)
-    #print(f"### action = {action}")
-    nextObs,reward,done,i = env.step(action) 
-    nextO_ = convertToIndex(nextObs)
-    print(f">> Action {env.javaGym.actionSpace[action]}, new position: {nextObs}, reward={reward}, terminal={done}")    
-    o = nextObs
-    k = k + 1
+seq = qalg.getRun(env,10,verbose=True)
 print("======")
+print(f"obtained run: {seq}")
 env.close()
 
+
+
+# ignore the stuffs below:)
+#
+#for y in range(N_plus_2):
+#    y_ = N_plus_2 - y - 1
+#    y__ = y_ - 1
+#    for x in range(N_plus_2) :
+#        x__ = x - 1
+#        index = x * N_plus_2 + y_ 
+#        vals = qalg.qtable[index]
+#        print(f"q({x__},{y__})={vals}")
 
 
--- python/src/japyre/qlearning.py
+++ python/src/japyre/qlearning.py
@@ -1,9 +1,31 @@
 import random
+from xmlrpc.client import Boolean
+import gym
 import numpy as np
 from typing import Any, Callable, Dict, List
 
 class Qlearning:
+    '''
+    An implementation of Q-learning, as a simple example of an RL-algorithm
+    to try on Java GymEnv. The algorithm maintains a Q-table, such that
+    for a state s and action a, Q(s,a) represents the value of executing the
+    action a on the state s.
+
+    We do not literally represent states and actions by e.g. names or some
+    complex data-type. Rather they are just indices.
+
+    The algorithm assumes finite number of states (NS) and actions (NA). 
+    A state is identified by an id in [0..NS), and similarly an action
+    is an id in [0..NA). The Q-table is represented by self.qtable, which
+    is a 2D array, such that to know the value of Q[s][a] we first need
+    to translate s and a to the corresponding index, say si and ai, and
+    the we inspect self.qtable[si][ai].
+    '''
     def __init__(self,numberOfStates,numberOfActions):
+        '''
+        Constructor. Specify the number of possible states and the number
+        of possible actions to consider.
+        '''
         self.numberOfStates  = numberOfStates
         self.numberOfActions = numberOfActions
         self.seed  = 127
@@ -14,28 +36,52 @@ class Qlearning:
         a = [ [ random.random()/100.0 for y in range(numberOfActions) ] for x in range(numberOfStates) ]
         self.qtable = np.array(a)
 
-    def maxActionValue(self,currentState:int) -> Dict :
+        self.stateIndexer  = lambda i : i
+        self.actionIndexer = lambda i : i
+
+    def maxActionValue(self,indexOfcurrentState:int) -> Dict :
+        '''
+        Given an index si of some state s, this returns a pair (a,v),
+        where a is the action (more precisely, it is an index of
+        an action), such that qtable[si][a] = max{qtable[si][b] | b is action},
+        and v is the value of qtable[si][a].
+        '''
         bestAction = 0
-        bestValue = self.qtable[currentState][0]
+        bestValue = self.qtable[indexOfcurrentState][0]
         for a in range(1,self.numberOfActions) :
-            v = self.qtable[currentState][a]
+            v = self.qtable[indexOfcurrentState][a]
             if v > bestValue :
                bestAction = a
                bestValue = v
         return { "bestAction" : bestAction, "bestValue" : bestValue }
 
 
-    def getNextTrainedAction(self, currentState:int) -> int :
-        ''' Return an action, that is the best according to the qtable. '''
-        best = self.maxActionValue(currentState)
+    def getNextTrainedAction(self, currentState) -> int :
+        ''' 
+        Return an action (more precisely, the index of the action), that is 
+        the best according to the qtable. 
+        '''
+        si = self.stateIndexer(currentState)
+        best = self.maxActionValue(si)
         return best["bestAction"]
 
     def applyReward(self, oldstate:int, action:int, newstate:int, reward:float) -> None :
-        valOldState = self.qtable[oldstate][action]
-        valNewState = self.maxActionValue(newstate)["bestValue"]
-        self.qtable[oldstate][action] = valOldState + self.alpha * (reward + self.gamma * valNewState - valOldState)
+        '''
+        Apply the Q-learning update: Q(s,a) = Q(s,a) + alpha(reward * max_b(Q(s',b)) - Q(s,a))
+        '''
+        oldSi = self.stateIndexer(oldstate)
+        newSi = self.stateIndexer(newstate)
+        ai = self.actionIndexer(action)
+        valOldState = self.qtable[oldSi][ai]
+        valNewState = self.maxActionValue(newSi)["bestValue"]
+        self.qtable[oldSi][ai] = valOldState + self.alpha * (reward + self.gamma * valNewState - valOldState)
         
-    def getNextAction(self, currentState:int) -> int :
+    def getNextAction(self, currentState) -> int :
+        '''
+        Select the next action (its index) to do (for learning). This can be the best action,
+        according to the current qtable, or just a random action, by some probability
+        epsilon.
+        '''
         if random.random() < self.exploreProbability :
             action = random.randint(0,self.numberOfActions-1)
             return action  
@@ -54,6 +100,68 @@ class Qlearning:
     def __str__(self) -> str:
         s = f"{self.qtable}"
         return s
+
+    def learn(self, env : gym.Env, maxNumberOfSteps:int, verbose:Boolean=False) -> None :
+        '''
+        Run the Q-learning algorithm on the given Gym-env, for some maximum number
+        of steps. Whenever a terminal state is encountered, the env will be reset
+        to initial state, and a new episode of learning is started. This goed on
+        until we reach the max-number of steps (totalled over all episodes).
+        '''
+        print("====== Learning ...")
+        k = 0
+        stepCountInEpisode = 0
+        episode = 0
+        o = env.reset()
+        while k < maxNumberOfSteps:
+            action = self.getNextAction(o)
+            #print(f"### action = {action}")
+            nextObs,reward,done,i = env.step(action) 
+            #print(f"### next={nextObs}, next_={nextO_}")
+            self.applyReward(o,action,nextObs,reward)
+            debugo = o
+            o = nextObs
+            if done :
+                if verbose:
+                    print(f">> Episode {episode}, #steps={stepCountInEpisode}, reward={reward}")
+                episode = episode + 1
+                o = env.reset()
+                stepCountInEpisode = 0
+                #if (reward > 0) : 
+                #    print(f"### o={debugo}, o_={o_}, next={nextObs}, next_={nextO_}, rw={reward}")
+                #    print(f"### updated enrty={qalg.qtable[o_][action]}")
+                #    break   
+            else:
+                stepCountInEpisode = stepCountInEpisode + 1
+            k = k + 1
+
+        print("====== Training has finished.")
+
+    def getRun(self, env : gym.Env, maxNumberOfSteps:int, verbose:Boolean=False) -> Dict :
+        '''
+        Return a sequence of actions (starting from the env's initial state) that
+        greedily choose each next action such that it is the action with the greatest
+        value on the current state according to the qtable.
+        '''
+        seq = []
+        k = 0
+        done = False
+        o = env.reset()
+        if verbose:
+            print(f">> initial state: {o}")    
+        while (not done) and k < maxNumberOfSteps :
+            #print(f"### o={o}, o_={o_}")
+            action = self.getNextTrainedAction(o)
+            #print(f"### action = {action}")
+            nextObs,reward,done,i = env.step(action) 
+            a = env.javaGym.actionSpace[action]
+            seq.append(a)
+            print(f">> Action {a}, new state: {nextObs}, reward={reward}, terminal={done}")    
+            o = nextObs
+            k = k + 1
+
+        return { "run":seq, "lastreward":reward, "terminal":done}
+
     
     
 # just for testing:
